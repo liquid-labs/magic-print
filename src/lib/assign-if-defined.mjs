dded6603a89ffa32b3185235ea80c89d0aed80b9
Zane Rockenbaugh <zane@liquid-labs.com>
2024-03-24 10:26:21 -0500

    Made undefined transparent to merge

--- src/lib/assign-if-defined.mjs
+++ src/lib/assign-if-defined.mjs
@@ -1,6 +1,10 @@
 const assignIfDefined = (...objects) => {
   const targetObject = objects[0]
   for (let i = 1; i < objects.length; i += 1) {
+    if (objects[i] === undefined || objects[i] === null) {
+      continue
+    }
+
     const subject = objects[i]
     for (const [key, value] of Object.entries(subject)) {
       if (value !== undefined) {
